4dba9a476720d1431a96d164d1b66bab05e4da9e
deepesh136 <deepesh.gupta136@gmail.com>
2018-06-10 16:04:44 +0530

    naukari.com data ready

--- naukri_scrapping.py
+++ naukri_scrapping.py
@@ -23,55 +23,92 @@ pass - adhoc@#@!123
 desktop_agents = ['Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.100 Safari/537.36']
 USER_AGENT = {'User-Agent': choice(desktop_agents)}
 
-try:
-	# Connect to the database
-	connection = pymysql.connect(host='localhost',
-        	                     user='root',
-        	                     password='',
-        	                     db='portaljobs',
-        	                     cursorclass=pymysql.cursors.DictCursor)
+#extracting urls for alphabetical order of company names
 
-	page = requests.get("https://www.naukri.com/top-company-jobs", headers= USER_AGENT, timeout=10)
-	if page.status_code==200:
-		soup = BeautifulSoup(page.text, 'lxml')
-		def browse_jobs_by_Companies(soup):
-			companies_urls = []
-			company_initials= []
+def browse_all_companies_list(companies_urls= []):
+	try:
+		page = requests.get("https://www.naukri.com/top-company-jobs", headers= USER_AGENT, timeout=10)
+		if page.status_code==200:
+			soup = BeautifulSoup(page.text, 'lxml')
 			for div in soup.find_all("div", attrs={"class":"tabs"}):
-				#print("loop1")
 				for a in div.find_all("a", attrs={"class":"tab"}):
-						company_initials.append(a.get_text())
+						#company_initials.append(a.get_text())
 						companies_urls.append(a.get('href'))
-
-			for i in range(1, len(companies_urls)):
-				page_next = requests.get(companies_urls[i], headers= USER_AGENT, timeout=10)
-				if page_next.status_code==200:
-					company_names = []
-					company_links = []
-					soup1 = BeautifulSoup(page_next.text, 'lxml')
-					for div2 in soup1.find_all("div", attrs={"id":"tabs_job"}):
-						for div3 in div2.find_all("div", attrs={"id":"tabJ-A"}):
-							for link in div3.find_all("a"):
-								company_names.append(link.get_text())
-								company_links.append(link.get('href'))
-					with open("naukri-companies.txt", 'a') as f:
-						for j in range(len(company_names)):
-							company = company_names[j]
-							comlink = company_links[j]
-							try:
-    								with connection.cursor() as cursor:
-									sql1 = "INSERT INTO company_urls (com_name, com_url) VALUES ("+company+", "+comlink+")"
-									cursor.execute(sql1)
-				else:
-					print("page2 error", page_next.status_code)
-		browse_jobs_by_Companies(soup)	
-	else:
-		print(page.status_code)	
+			companies_urls.pop(0)
+			return browse_company_names(companies_urls)
+		else:
+			return browse_all_companies_list()	
 
 
-except Exception as e:
-	print(str(e))
+	except Exception as e:
+		return str(e)
 
-finally:
-	time.sleep(20)
-	print("timeout")
+	finally:
+		time.sleep(20)
+		return "timeout"
+
+#extracting all company names and links of companies on naukri
+
+def browse_company_names(companies_urls, company_names = [], company_links = []):
+	for urls_company in companies_urls:
+		try:
+			page_next = requests.get(urls_company, headers= USER_AGENT, timeout=10)
+			if page_next.status_code==200:
+				soup1 = BeautifulSoup(page_next.text, 'lxml')
+				for div2 in soup1.find_all("div", attrs={"id":"tabs_job"}):
+					for div3 in div2.find_all("div", attrs={"id":"tabJ-A"}):
+						for link in div3.find_all("a"):
+							company_names.append(link.get_text())
+							company_links.append(link.get('href'))
+		except:
+			print("exception occured")
+	return jobs_list_by_companies(company_links)
+
+#entering companie's naukri urls for job list
+
+def jobs_list_by_companies(company_links, company_jobs_urls= [], job_title= [], job_location= []):
+	for job_search in company_links:
+		try:
+			page_company = requests.get(job_search, headers= USER_AGENT, timeout=10)
+			if page_company.status_code==200:
+				soup2 = BeautifulSoup(page_company.text, 'lxml')
+				for div_joblist in soup2.find_all("div", attrs={"class":"row"}):
+					for a_joblist in div_joblist.find_all("a", attrs={"class":"content"}):
+						company_jobs_urls.append(a_joblist.get('href'))
+						for job_opening in a_joblist.find_all('li', attrs= {"class":"desig"}):
+							job_title.append(job_opening.get('title'))
+						for span in a_joblist.find_all('span', attrs= {"class":"loc"}):
+							job_location.append(span.get_text())
+			else:
+				print(page_company.status_code)
+		except Exception as e2:
+			print(str(e2))
+	return job_title, job_location, jobs_details(company_jobs_urls)
+	
+
+#opening jobs list and extracting details of job
+
+def jobs_details(company_jobs_urls, more_info=[], skills= []):
+	for jobs_by_companies in company_jobs_urls:
+		try:
+			page_jobs = requests.get(jobs_by_companies, headers= USER_AGENT, timeout=10)
+			if page_jobs.status_code==200:
+				soup3= BeautifulSoup(page_jobs.text, 'lxml')
+				div_description= soup3.find("div", attrs={"class":"JD"})
+				ul= div_description.find('ul', attrs= {"class":"listing mt10 wb"})
+				description= ul.get_text()
+				div2_description= div_description.find('div', attrs= {"class":"jDisc mt20"})
+				for p in div2_description.find_all('p'):
+					more_info.append(p.get_text())
+				div3_skills= div_description.find('div', attrs= {"class":"ksTags"})
+				for a_description in div3_skills.find_all('a'):
+					skills.append(a_description.get_text())
+			else:
+				return page_jobs.status_code
+		except Exception as e3:
+			return str(e3)
+	return description, more_info, skills
+				
+		
+browse_all_companies_list()
+
